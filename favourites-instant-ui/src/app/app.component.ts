24500e8d3f511e476050ad4cae628e4e6709b077
Damuja <dmuja247@gmail.com>
2024-11-01 10:15:41 +0000

    title change

--- favourites-instant-ui/src/app/app.component.ts
+++ favourites-instant-ui/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { DataGridComponent } from './data-grid/data-grid.component';
   standalone: true,
   imports: [DataGridComponent],
   template: `
-    <h1>Angular Data Grid with Search</h1>
+    <h1>Dummy Fav & Instant tickets</h1>
     <app-data-grid></app-data-grid>
   `,
   styleUrls: ['./app.component.css'],
